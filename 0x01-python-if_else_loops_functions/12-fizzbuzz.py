40d7384f4b45b5881c74bc4ac4c630b8970d9e66
Nzparra <nzparra@gmail.com>
2019-12-10 22:48:37 +0000

    [Add]Excersice #12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    a = "Fizz"
+    b = "Buzz"
+    for i in range(1, 100):
+        if (i % 3 == 0 and i % 5 == 0):
+            print("{}{}".format(a, b), end=' ')
+        else:
+            if(i % 3 == 0):
+                print("{}".format(a), end=' ')
+            elif(i % 5 == 0):
+                print("{}".format(b), end=' ')
+            else:
+                print("{:d}".format(i), end=' ')
