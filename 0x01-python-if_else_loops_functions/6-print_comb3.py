a225ff2fd9b794faeac53e4d3c6ff8cd67136085
Nzparra <nzparra@gmail.com>
2019-12-10 19:15:09 +0000

    [Add]Fix Pep

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for d in range(0, 10):
         if i != d and i < d and i != 8:
-            print("{:d}{:d}".format(i, d), end = ', ')
+            print("{:d}{:d}".format(i, d), end=', ')
         if i == 8 and i < d:
             print("{:d}{:d}".format(i, d))
