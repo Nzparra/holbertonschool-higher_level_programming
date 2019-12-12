d95837466ac030ffa5e73ec0ee3fb20229f715ee
Nzparra <nzparra@gmail.com>
2019-12-12 17:08:19 +0000

    [Update]Fix 1 argument case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     import sys
     l = len(sys.argv)
     if l - 1 > 0:
-        print("{:d} arguments:".format(l - 1))
+        if l == 2:
+            print("{:d} argument:".format(l - 1))
+        else:
+            print("{:d} arguments:".format(l - 1))
     else:
         print("0 arguments.")
     for i in range(1, l):
