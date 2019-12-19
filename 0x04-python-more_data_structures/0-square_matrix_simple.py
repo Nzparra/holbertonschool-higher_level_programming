00e1bbc055aa4f9628e96be0b28010d4ce899e98
Nzparra <nzparra@gmail.com>
2019-12-19 19:13:09 +0000

    [Fix]Update a for

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x**x, i)) for i in matrix]
+    new = []
+    for i in matrix:
+        new.append(list(map(lambda j: j **2, i)))
+    return new
