e69ac127ae4d6ae06501a7c63f60b26d7b31fdb0
Nzparra <nzparra@gmail.com>
2019-12-19 19:34:20 +0000

    [FIX]Uppercase in a if statement

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_String, str) is False:
+    if roman_string is None or isinstance(roman_string, str) is False:
         return 0
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     value = 0
