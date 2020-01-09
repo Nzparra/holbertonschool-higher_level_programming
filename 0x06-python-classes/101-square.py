aeed50012db1fe1d92e6d030cff96f17f5fa5d8f
Nzparra <nzparra@gmail.com>
2020-01-09 22:49:09 +0000

    [Add]Square instance

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(value[0]) and type(value[1]) != int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
+
+    def area(self):
+        return(self.__size * self.__size)
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print("{}{}".format(' '*self.__position[0], '#'*self.__size))
+
+    def __repr__(self):
+        string = ""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                string += "\n"
+            for i in range(self.size):
+                for c in range(self.__position[0]):
+                    string += " "
+                for c in range(self.__size):
+                    string += "#"
+                string += "\n"
+        return string[:-1]
