60179d9b92ac346c0aac82dfedd07b8584765d89
Nzparra <44713365+Nzparra@users.noreply.github.com>
2020-01-21 18:34:27 -0500

    Fix negative value

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
     with open(filename, encoding="utf-8") as file:
-        if (nb_lines == 0):
+        if (nb_lines <= 0):
             print(file.read(), end="")
         else:
             for line in file:
