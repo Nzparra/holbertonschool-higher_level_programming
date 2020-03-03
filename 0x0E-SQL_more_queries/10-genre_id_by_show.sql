fcbc519c0b3f1f1d54495bbda333302237b554a2
Nicolas <nzparra@gmail.com>
2020-03-03 17:51:17 -0500

    [Change]Order BY

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,4 @@
 -- that lists all shows contained in hbtn_0d_tvshows
-SELECT shows.title, genre.genre_id
-FROM tv_shows as shows
-INNER JOIN tv_show_genres as genre
-ON shows.id = genre.show_id
+SELECT shows.title, genre.genre_id FROM tv_shows as shows
+JOIN tv_show_genres as genre ON shows.id = genre.show_id
 ORDER BY shows.title, genre.genre_id ASC;
