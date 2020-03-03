9b76676d2ce16c3b51a3f9fb6d298c73eb61d9fa
Nicolas <nzparra@gmail.com>
2020-03-03 18:00:45 -0500

    [Change]ALiases

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- that lists all tv_shows contained in hbtn_0d_tvtv_shows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY 1, 2;
