9c6a8ec76db57ebd8e8edf0b5f141383cc2a0bd6
Nicolas <nzparra@gmail.com>
2020-03-30 02:48:20 -0500

    [Fix]Collate N

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     db = sql.connect(host='localhost', port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
     sq = db.cursor()
-    sq.execute("SELECT * FROM states WHERE name='{:s}' ORDER BY id ASC"
+    sq.execute("SELECT * FROM states WHERE name='{:s}'\
+               COLLATE latin1_general_cs ORDER BY id ASC"
                .format(sys.argv[4]))
     for row in sq:
         print("{}".format(row))
