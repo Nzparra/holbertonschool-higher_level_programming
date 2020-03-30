43d2267fa953e0d9bdd446b89d7e1ec15ab337af
Nicolas <nzparra@gmail.com>
2020-03-30 02:46:02 -0500

    [Fix]Collate N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = sql.connect(host='localhost', port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
     sq = db.cursor()
-    sq.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    sq.execute("SELECT * FROM states WHERE name COLLATE latin1_general_cs\
+               LIKE 'N%' ORDER BY id ASC")
     sqq = sq.fetchall()
     for row in sqq:
         print("{}".format(row))
