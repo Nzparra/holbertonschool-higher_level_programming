d9051ddbbe25b848d67fd133c6a1a5e9166a926e
Nzparra <nzparra@gmail.com>
2020-05-12 13:25:41 -0500

    [FIX] Show error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,7 @@ const rqs = require('request');
 rqs(process.argv[2], function (err, response) {
   if (err == null) {
     console.log('code:' + response.statusCode);
+  } else {
+    console.log(err);
   }
 });
