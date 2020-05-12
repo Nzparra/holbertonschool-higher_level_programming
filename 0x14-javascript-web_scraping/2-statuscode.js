03b7a93e9f7c2a6b529f3abfdb25a94a27eed9e5
Nzparra <nzparra@gmail.com>
2020-05-12 13:37:20 -0500

    [Update]Semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const url = process.argv[2];
 const request = require('request');
-request(url, function (error, response){
-    if (error) {
-        console.error('error:', error);
-    } else {
-        console.log(`code: ${response.statusCode}`);
-    }
+request(url, function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
